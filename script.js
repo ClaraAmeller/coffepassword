8d108e17f59141e52e998d4070dc03b13675aacb
Clara <claraameller7@gmail.com>
2016-08-20 19:31:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function howMany() {
+// 					document.getElementById("how_many").innerHTML = 
+// 						"<input style="text" id="userInput" value="How many characters"/>
+// 						<input type="submit" onclick="random()" value="Go"/>";
+// 				}
+
+// 				function random() {
+// 					var max = document.getElementById("userInput").value;
+// 					alert(max);
+// 				}
+
+
+    // Enables animation for the selected item
+    $('#animated-switch').bootstrapSwitch('setAnimated', true);
+    // Disables animation for the selected item
+    $('#animated-switch').bootstrapSwitch('setAnimated', false);
